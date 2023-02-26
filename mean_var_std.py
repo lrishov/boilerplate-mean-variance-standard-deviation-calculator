c3cfb877a16119ae5d77ae308d0b3df9e7c0214c
lrishov <51161299+lrishov@users.noreply.github.com>
2023-02-26 14:47:09 +0300

    Update mean_var_std.py

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,16 @@
 import numpy as np
 
 def calculate(list):
-
-
-
+    if len(list) < 9:
+        raise ValueError("List must contain nine numbers.")
+   
+    sarray = np.array(list).reshape((3,3))
+    calculations = {}
+    calculations['mean'] = [sarray.mean(axis = x).tolist() for x in [0, 1, None]]
+    calculations['variance'] = [sarray.var(axis = x).tolist() for x in [0, 1, None]]
+    calculations['standard deviation'] = [sarray.std(axis = x).tolist() for x in [0, 1, None]]
+    calculations['max'] = [sarray.max(axis = x).tolist() for x in [0, 1, None]]
+    calculations['min'] = [sarray.min(axis = x).tolist() for x in [0, 1, None]]
+    calculations['sum'] = [sarray.sum(axis = x).tolist() for x in [0, 1, None]]
 
     return calculations
